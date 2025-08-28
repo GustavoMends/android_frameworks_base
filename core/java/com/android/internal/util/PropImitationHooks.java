ba33eedd9c74867a770a77c01849f635d3cf4c10
Gustavo Mendes <gusttavo.me@outlook.com>
2025-08-28 13:39:06 -0300

    PIHooks: Drop ARCore spoof
    Historically, the ARCore fingerprint spoof
    was introduced during the Pixel Props Utils days.
    
    PPU used the Pixel fingerprint for all Google apps.
    However, ARCore needs to detect the device's actual model
    in order to work properly.
    
    We now use PIHooks, which only spoofs predefined apps.
    Additionally, most devices use the stock fingerprint by default.
    
    Therefore, this ARCore spoof is completely useless and
    can be safely removed.
    
    Change-Id: I0885a93090156eafc33076e460ee75e76717a88a
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -66,7 +66,6 @@ public class PropImitationHooks {
 
     private static final String DATA_FILE = "gms_certified_props.json";
 
-    private static final String PACKAGE_ARCORE = "com.google.ar.core";
     private static final String PACKAGE_FINSKY = "com.android.vending";
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PROCESS_GMS_UNSTABLE = PACKAGE_GMS + ".unstable";
@@ -117,7 +116,7 @@ public class PropImitationHooks {
                     "PIXEL_2025_MIDYEAR_EXPERIENCE");
 
     private static volatile List<String> sCertifiedProps = new ArrayList<>();
-    private static volatile String sStockFp, sNetflixModel;
+    private static volatile String sNetflixModel;
 
     private static volatile String sProcessName;
     private static volatile boolean sIsGms, sIsFinsky, sIsPhotos;
@@ -137,7 +136,6 @@ public class PropImitationHooks {
             return;
         }
 
-        sStockFp = res.getString(R.string.config_stockFingerprint);
         sNetflixModel = res.getString(R.string.config_netflixSpoofModel);
 
         sProcessName = processName;
@@ -146,7 +144,6 @@ public class PropImitationHooks {
         sIsPhotos = packageName.equals(PACKAGE_GPHOTOS);
 
         /* Set Certified Properties for GMSCore
-         * Set Stock Fingerprint for ARCore
          * Set custom model for Netflix
          * Set Pixel XL for Google Photos
          */
@@ -156,9 +153,6 @@ public class PropImitationHooks {
             } else {
                 dlog("Not setting Play Integrity props in isolated process");
             }
-        } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
-            dlog("Setting stock fingerprint for: " + packageName);
-            setPropValue("FINGERPRINT", sStockFp);
         } else if (sIsPhotos) {
             dlog("Spoofing Pixel 1 for Google Photos");
             sPixelOneProps.forEach((PropImitationHooks::setPropValue));
